1890367ed5d99200a84942ac65f4837b19d09298
philippe44 <philippe44@users.noreply.github.com>
2022-10-08 19:35:09 -0700

    better windows script

--- build.cmd
+++ build.cmd
@@ -2,16 +2,32 @@ setlocal
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
 
-REM cd flac && mkdir build && cd build
-REM cmake .. -A Win32 -DOGG_LIBRARY=..\..\ogg -DOGG_INCLUDE_DIR=..\..\ogg\include -DINSTALL_MANPAGES=OFF
-REM cd ..
+if /I [%1] == [rebuild] (
+	rd /q /s flac\build
+	rd /q /s ogg\build
+)
 
-REM cd ogg && mkdir build && cd build
-REM cmake .. -A Win32 
-REM cd ..
+if not exist ogg\build (
+	mkdir ogg\build
+	cd ogg\build
+	cmake .. -A Win32
+	cd ..\..
+)	
+
+if not exist flac\build (
+	mkdir flac\build
+	cd flac\build
+	cmake .. -A Win32 -DOGG_LIBRARY=..\..\ogg -DOGG_INCLUDE_DIR=..\..\ogg\include -DINSTALL_MANPAGES=OFF
+	cd ..\..
+)	
 
 set config=Release
-msbuild libcodecs.sln /property:Configuration=%config% %1
+
+if /I [%1] == [rebuild] (
+	msbuild libcodecs.sln /property:Configuration=%config% -t:rebuild
+) else (
+	msbuild libcodecs.sln /property:Configuration=%config%
+)
 
 set target=targets\win32\x86
 set include=targets\include
@@ -22,12 +38,15 @@ if exist %target% (
 
 robocopy lib\x86 %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
 robocopy .libs %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
+
 robocopy flac\build\src\libFLAC\%config% %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
 robocopy flac\build\src\share\utf8\%config% %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
+robocopy ogg\build\%config% %target% *.lib /NDL /NJH /NJS /nc /ns /np
 
-robocopy ogg\include %include%\ogg *.h /NDL /NJH /NJS /nc /ns /np
-robocopy alac\codec %include%\alac ALAC*.h /NDL /NJH /NJS /nc /ns /np
+rem flac & ogg don't seem to really have per-platform different config files (thanks stdint)
 robocopy flac\include %include%\flac *.h /S /NDL /NJH /NJS /nc /ns /np /XD test*
+robocopy ogg\include %include%\ogg *.h /S /NDL /NJH /NJS /nc /ns /np
+robocopy alac\codec %include%\alac ALAC*.h /NDL /NJH /NJS /nc /ns /np
 robocopy shine\src\lib %include%\shine layer3.h /NDL /NJH /NJS /nc /ns /np
 
 lib.exe /OUT:%target%/libcodecs.lib %target%/*.lib
