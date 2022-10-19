682fcdd944284d90c7e20d69be8e2a1ada891a8a
philippe44 <philippe44@users.noreply.github.com>
2022-10-18 20:34:14 -0700

    pdb were missing

--- build.cmd
+++ build.cmd
@@ -12,6 +12,7 @@ if /I [%1] == [rebuild] (
 	rd /q /s flac\%build%
 	rd /q /s ogg\%build%
 	rd /q /s soxr\%build%
+	rd /q /s vorbis\%build%
 )
 
 if not exist ogg\%build% (
@@ -35,6 +36,13 @@ if not exist soxr\%build% (
 	cd ..\..
 )	
 
+if not exist vorbis\%build% (
+	mkdir vorbis\%build%
+	cd vorbis\%build%
+	cmake .. -A Win32 -DOGG_LIBRARY=..\..\ogg -DOGG_INCLUDE_DIR=..\..\ogg\include -DINSTALL_MANPAGES=OFF
+	cd ..\..
+)	
+
 msbuild libcodecs.sln /property:Configuration=%config% %option%
 
 if exist %target% (
@@ -48,14 +56,16 @@ robocopy .libs %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
 REM this takes care of flac, pgg, soxr
 robocopy flac\%build%\src\libFLAC\%config% %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
 robocopy flac\%build%\src\share\utf8\%config% %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
-robocopy ogg\%build%\%config% %target% *.lib /NDL /NJH /NJS /nc /ns /np
-robocopy soxr\%build%\src\%config% %target% *.lib /NDL /NJH /NJS /nc /ns /np
-robocopy addons\build %target% *.lib /NDL /NJH /NJS /nc /ns /np
+robocopy ogg\%build%\%config% %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
+robocopy vorbis\%build%\lib\%config% %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
+robocopy soxr\%build%\src\%config% %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
+robocopy addons\build %target% *.lib *.pdb /NDL /NJH /NJS /nc /ns /np
 
 rem flac & ogg don't seem to really have per-platform different config files (thanks stdint)
 robocopy flac\include %include%\flac *.h /S /NDL /NJH /NJS /nc /ns /np /XD test*
 robocopy ogg\include %include%\ogg *.h /S /NDL /NJH /NJS /nc /ns /np
-robocopy soxr\src %include%\soxr soxr.h /S /NDL /NJH /NJS /nc /ns /np
+robocopy vorbis\include %include%\vorbis *.h /S /NDL /NJH /NJS /nc /ns /np
+robocopy soxr\src %include%\soxr soxr.h /NDL /NJH /NJS /nc /ns /np
 robocopy mad %include%\mad mad.h /NDL /NJH /NJS /nc /ns /np
 robocopy alac\codec %include%\alac ALAC*.h /NDL /NJH /NJS /nc /ns /np
 robocopy shine\src\lib %include%\shine layer3.h /NDL /NJH /NJS /nc /ns /np
