89e681ebe1e22aa3db669465fac4f25076ff1585
philippe44 <philippe44@users.noreply.github.com>
2022-10-24 23:18:30 -0700

    remove stack smashing detection for flac (because of solaris)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 list="x86_64-linux-gnu-gcc x86-linux-gnu-gcc arm-linux-gnueabi-gcc aarch64-linux-gnu-gcc sparc64-linux-gnu-gcc mips-linux-gnu-gcc powerpc-linux-gnu-gcc x86_64-macos-darwin-gcc x86_64-freebsd-gnu-gcc x86_64-solaris-gnu-gcc"
 declare -A alias=( [x86-linux-gnu-gcc]=i686-linux-gnu-gcc [x86_64-macos-darwin-gcc]=x86_64-apple-darwin19-gcc [x86_64-freebsd-gnu-gcc]=x86_64-gnu-freebsd13.1-gcc [x86_64-solaris-gnu-gcc]=x86_64-gnu-solaris2.x-gcc )
-declare -A cppflags=( [mips-linux-gnu-gcc]="-march=mips32" [powerpc-linux-gnu-gcc]="-m32")
+declare -A cppflags=( [mips-linux-gnu-gcc]="-march=mips32" [powerpc-linux-gnu-gcc]="-m32" )
 declare -a compilers
 
 IFS= read -ra candidates <<< "$list"
@@ -169,6 +169,7 @@ do
 	item=flac	
 	if [ ! -f $target/libFLAC-static.a ] || [[ -n $clean ]]; then
 		cd $item
+		# solaris does not have the stack protection primitives
 		./configure --disable-stack-smash-protection --enable-debug=no --enable-static --disable-shared --with-ogg-includes=$pwd/targets/include/ogg --with-ogg-libraries=$pwd/$target --disable-cpplibs --disable-oggtest --host=$platform-$host 
 		make clean && make -j8
 		cd $pwd
